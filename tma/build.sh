b961f1b2b4e0b6371bb04cc599cc8e8489b82b54
Basit Ayantunde <rlamarrr@gmail.com>
2026-01-28 13:55:42 +0000

    update

--- tma/build.sh
+++ tma/build.sh
@@ -1,4 +1,22 @@
-docker run --gpus=all --name ecstatic-monk -it -d rapidsai/devcontainers:26.02-cpp-cuda13.0-ubuntu24.04
-docker attach ecstatic-monk 
-sudo apt update && sudo apt upgrade
+CONTAINER_VERSION="26.02"
+CUDA_VERSION="13.0"
+
+function create_docker_container(){
+    CONTAINER_TAG="rapidsai/devcontainers:${CONTAINER_VERSION}-cpp-cuda${CUDA_VERSION}-ubuntu24.04"
+    CONTAINER_NAME=$1
+    docker run -it \
+    --privileged \
+    --mount type=bind,src=$HOME/.bash_history,dst=/home/.bash_history,consistency=consistent \
+    --mount type=bind,src=$HOME/cuda-experiments,dst=/home/coder/cuda-experiments,consistency=consistent \
+    --mount type=bind,src=$HOME/.config,dst=/home/coder/.config,consistency=consistent \
+    --gpus=all \
+    --name $CONTAINER_NAME \
+    -d $CONTAINER_TAG /bin/bash
+}
+
+CONTAINER_NAME="ecstatic"
+
+create_docker_container $CONTAINER_NAME
+docker attach $CONTAINER_NAME
+
 
